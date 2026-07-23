191f3947742cb88db0cecb653f1ef26e3bdee426
cauriovieiradacunha40 <cauriov3@gmail.com>
2026-07-23 06:09:29 -0300

    feat: create login page structure

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,13 +1,42 @@
+import Logo from '../../assets/logo.svg';
+import {
+  Container,
+  LeftContainer,
+  RightContainer,
+  Title,
+  Form,
+  InputContainer,
+  Link,
+  Button,
+} from './styles';
 
 export function Login() {
-    return (
-        <div>
-            <h1>Login</h1> 
-        </div>
-    )
+  return (
+    <Container>
+      <LeftContainer>
+        <img src={Logo} alt="Logo-devburger" />
+      </LeftContainer>
 
+      <RightContainer>
+        <Title>
+          Olá, seja bem vindo ao <span>Dev Burguer!</span>
+          Acesse com seu <span>Login e senha.</span>
+        </Title>
+        <Form>
+          <InputContainer>
+            <label>Email</label>
+            <input type="email" />
+          </InputContainer>
+
+          <InputContainer>
+            <label>Senha</label>
+            <input type="password" />
+          </InputContainer>
+          <Link>Esqueci minha senha</Link>
+          <Button>Entrar</Button>
+        </Form>
+        <Link>Nâo possui conta? Clique aqui</Link>
+      </RightContainer>
+    </Container>
+  );
 }
-
-
-
-
